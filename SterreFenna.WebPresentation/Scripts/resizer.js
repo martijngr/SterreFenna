55f9bda7c0c66a5c2428384d17d9bda0f6da99a7
MartijnG <martijngr@gmail.com>
2016-08-28 12:28:51 +0200

    Calculate content height onload and onresize

---
+++ SterreFenna.WebPresentation/Scripts/resizer.js
@@ -0,0 +1,10 @@
+﻿function calcuteContentHeight() {
+    var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var headerHeight = $("header").outerHeight(true);
+    var footerheight = 135;
+    var contentDiv = $(".swiper-container");
+
+    var contentHeight = viewportHeight - headerHeight - footerheight;
+    contentDiv.height(contentHeight);
+}
